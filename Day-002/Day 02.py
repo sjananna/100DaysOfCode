fee939b5049ea263a819e9db7bf19d85379787f5
sjananna <samiajahan105@gmail.com>
2026-09-13 19:58:23 -0400

    Day 2: learned input functions and variables

---
+++ Day-002/Day 02.py
@@ -0,0 +1,26 @@
+# Input Functions = It is a function which is a verb or actions that the computer or computer language will already know about it
+input("Whats your name?")
+print("My name is Samia")
+
+
+#Variable = A variable is a container for a value within your own program
+#When "=" is added in the program it will work as a special assignment operator. This equal sign literally will assign what is on the right to what is on the left. That's why value returned by 'input' function is assigned to the 'name' variable
+name = input("Whats your name?")
+print("My name is Samia")
+
+# Further editing our code, you could type
+#Ask the user for their name and store whatever they type inside the variable name.
+name = input("What's your name? ")
+print("hello, ")
+# The variable must be assigned a value before you try to use or print it.
+name = "Samia"
+print(name)
+
+
+#Ask user for their name
+name = input("What's your name? ")
+name = "Samia"
+print("hello, " + name)
+
+
+
